754db87973c1d1f720e402264a9738a96d3ad731
Lloyd Garrison <lloyd.h.garrison@gmail.com>
2016-10-13 16:27:34 -0500

    Adding objects_json.html exercise.

--- js/practice.js
+++ js/practice.js
@@ -57,7 +57,8 @@ function comboNations(n) {
 // Expected Output : 'abeemrstw'
 // Assume punctuation and numbers symbols are not included in the passed string.
 
-//
+"webmaster".join
+
 // TODO 5. Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string in upper case. Go to the editor
 // Example string : 'the quick brown fox'
 // Expected Output : 'The Quick Brown Fox '
