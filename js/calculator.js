d60c674d5a399eb9cb9fda363452163cc2a8795c
Lloyd Garrison <lloyd.h.garrison@gmail.com>
2016-10-20 14:16:19 -0500

    (99% finished) calculator project - still need to do percent button and merge functions (i.e. Numberpad, operators, etc.) Google API.

--- js/calculator.js
+++ js/calculator.js
@@ -4,259 +4,260 @@
 "use strict";
 
 
-    var lefty = document.getElementById("displayLeft");
-    var middle = document.getElementById("displayMiddle");
-    var righty = document.getElementById("displayRight");
-    var operator = ["+", "-", "*", "/"];
+var lefty = document.getElementById("displayLeft");
+var middle = document.getElementById("displayMiddle");
+var righty = document.getElementById("displayRight");
+var operator = ["+", "-", "*", "/"];
+var numberPad = document.getElementsByClassName("numPad");
 
-    var clear = document.getElementById('clears')
-    .addEventListener('click', function () {
-        lefty.value = ("");
-        middle.value = ("");
-        righty.value = ("");
+var clear = document.getElementById('clears')
+.addEventListener('click', function () {
+    lefty.value = ("");
+    middle.value = ("");
+    righty.value = ("");
+});
+
+
+
+document.getElementById('one')
+    .addEventListener('click', function() {
+        if (middle.value == "") {
+            lefty.value = (lefty.value + this.innerHTML)
+        } else if(middle.value == "="){
+            lefty.value = ("");
+            middle.value = ("");
+            righty.value = ("");
+            lefty.value = (lefty.value + this.innerHTML)
+        }else {
+            righty.value = (righty.value + this.innerHTML)
+        }
     });
 
+// document.getElementById('two')
+//     .addEventListener('click', function() {
+//         if (middle.value == "") {
+//             lefty.value = (lefty.value + this.innerHTML)
+//         } else if(middle.value == "="){
+//             lefty.value = ("");
+//             middle.value = ("");
+//             righty.value = ("");
+//             lefty.value = (lefty.value + this.innerHTML)
+//         }else {
+//             righty.value = (righty.value + this.innerHTML)
+//         }
+//     });
+//
+// document.getElementById('three')
+//     .addEventListener('click', function() {
+//         if (middle.value == "") {
+//             lefty.value = (lefty.value + this.innerHTML)
+//         } else if(middle.value == "="){
+//             lefty.value = ("");
+//             middle.value = ("");
+//             righty.value = ("");
+//             lefty.value = (lefty.value + this.innerHTML)
+//         }else {
+//             righty.value = (righty.value + this.innerHTML)
+//         }
+//     });
+//
+// document.getElementById('four')
+//     .addEventListener('click', function() {
+//         if (middle.value == "") {
+//             lefty.value = (lefty.value + this.innerHTML)
+//         } else if(middle.value == "="){
+//             lefty.value = ("");
+//             middle.value = ("");
+//             righty.value = ("");
+//             lefty.value = (lefty.value + this.innerHTML)
+//         }else {
+//             righty.value = (righty.value + this.innerHTML)
+//         }
+//     });
+//
+// document.getElementById('five')
+//     .addEventListener('click', function() {
+//         if (middle.value == "") {
+//             lefty.value = (lefty.value + this.innerHTML)
+//         } else if(middle.value == "="){
+//             lefty.value = ("");
+//             middle.value = ("");
+//             righty.value = ("");
+//             lefty.value = (lefty.value + this.innerHTML)
+//         }else {
+//             righty.value = (righty.value + this.innerHTML)
+//         }
+//     });
+// document.getElementById('six')
+//     .addEventListener('click', function() {
+//         if (middle.value == "") {
+//             lefty.value = (lefty.value + this.innerHTML)
+//         } else if(middle.value == "="){
+//             lefty.value = ("");
+//             middle.value = ("");
+//             righty.value = ("");
+//             lefty.value = (lefty.value + this.innerHTML)
+//         }else {
+//             righty.value = (righty.value + this.innerHTML)
+//         }
+//     });
+//
+// document.getElementById('seven')
+//     .addEventListener('click', function() {
+//         if (middle.value == "") {
+//             lefty.value = (lefty.value + this.innerHTML)
+//         } else if(middle.value == "="){
+//             lefty.value = ("");
+//             middle.value = ("");
+//             righty.value = ("");
+//             lefty.value = (lefty.value + this.innerHTML)
+//         }else {
+//             righty.value = (righty.value + this.innerHTML)
+//         }
+//     });
+//
+// document.getElementById('eight')
+//     .addEventListener('click', function() {
+//         if (middle.value == "") {
+//             lefty.value = (lefty.value + this.innerHTML)
+//         } else if(middle.value == "="){
+//             lefty.value = ("");
+//             middle.value = ("");
+//             righty.value = ("");
+//             lefty.value = (lefty.value + this.innerHTML)
+//         }else {
+//             righty.value = (righty.value + this.innerHTML)
+//         }
+//     });
+//
+// document.getElementById('nine')
+//     .addEventListener('click', function() {
+//         if (middle.value == "") {
+//             lefty.value = (lefty.value + this.innerHTML)
+//         } else if(middle.value == "="){
+//             lefty.value = ("");
+//             middle.value = ("");
+//             righty.value = ("");
+//             lefty.value = (lefty.value + this.innerHTML)
+//         }else {
+//             righty.value = (righty.value + this.innerHTML)
+//         }
+//     });
 
-
-    document.getElementById('one')
-        .addEventListener('click', function() {
-            if (middle.value == "") {
+document.getElementById('zero')
+    .addEventListener('click', function() {
+        if (middle.value == "") {
+            if(lefty.value.charAt(0) != "0") {
                 lefty.value = (lefty.value + this.innerHTML)
-            } else if(middle.value == "="){
-                lefty.value = ("");
-                middle.value = ("");
-                righty.value = ("");
+            }else if (lefty.value.charAt(0) == "0" && lefty.value.charAt(1) > "0") {
                 lefty.value = (lefty.value + this.innerHTML)
-            }else {
+            }
+        } else if(middle.value == "="){
+            lefty.value = ("");
+            middle.value = ("");
+            righty.value = ("");
+            lefty.value = (lefty.value + this.innerHTML)
+        }else {
+            if(righty.value.charAt(0) != "0") {
+                righty.value = (righty.value + this.innerHTML)
+            }else if (righty.value.charAt(0) == "0" && righty.value.charAt(1) > "0") {
                 righty.value = (righty.value + this.innerHTML)
             }
-        });
+        }
+    });
 
-    document.getElementById('two')
-        .addEventListener('click', function() {
-            if (middle.value == "") {
-                lefty.value = (lefty.value + this.innerHTML)
-            } else if(middle.value == "="){
-                lefty.value = ("");
-                middle.value = ("");
-                righty.value = ("");
-                lefty.value = (lefty.value + this.innerHTML)
-            }else {
-                righty.value = (righty.value + this.innerHTML)
+document.getElementById('decimal')
+    .addEventListener('click', function() {
+        if (middle.value == "") {
+            if(lefty.value % 1 == 0) {
+                lefty.value = (lefty.value + this.innerHTML);
             }
-        });
+        } else {
+            if(righty.value % 1 == 0) {
+                righty.value = (righty.value + this.innerHTML);
+            }
+        }
+    });
 
-    document.getElementById('three')
-        .addEventListener('click', function() {
-            if (middle.value == "") {
-                lefty.value = (lefty.value + this.innerHTML)
-            } else if(middle.value == "="){
-                lefty.value = ("");
-                middle.value = ("");
-                righty.value = ("");
-                lefty.value = (lefty.value + this.innerHTML)
-            }else {
-                righty.value = (righty.value + this.innerHTML)
-            }
-        });
+var adds = document.getElementById("addition")
+    .addEventListener('click', function () {
+        var RV = parseFloat(righty.value);
+        var LV = parseFloat(lefty.value);
+        middle.value = this.innerHTML;
+        if(lefty.value != "" && righty.value != "" && middle.value != "") {
+            lefty.value = RV + LV;
+            righty.value = "";
+        }
+    });
 
-    document.getElementById('four')
-        .addEventListener('click', function() {
-            if (middle.value == "") {
-                lefty.value = (lefty.value + this.innerHTML)
-            } else if(middle.value == "="){
-                lefty.value = ("");
-                middle.value = ("");
-                righty.value = ("");
-                lefty.value = (lefty.value + this.innerHTML)
-            }else {
-                righty.value = (righty.value + this.innerHTML)
-            }
-        });
+var minus = document.getElementById("subtraction")
+    .addEventListener('click', function () {
+        var RV = parseFloat(righty.value);
+        var LV = parseFloat(lefty.value);
+        middle.value = this.innerHTML;
+        if(lefty.value != "" && righty.value != "" && middle.value != "") {
+            lefty.value = LV - RV;
+            righty.value = "";
+        }
+    });
 
-    document.getElementById('five')
-        .addEventListener('click', function() {
-            if (middle.value == "") {
-                lefty.value = (lefty.value + this.innerHTML)
-            } else if(middle.value == "="){
-                lefty.value = ("");
-                middle.value = ("");
-                righty.value = ("");
-                lefty.value = (lefty.value + this.innerHTML)
-            }else {
-                righty.value = (righty.value + this.innerHTML)
-            }
-        });
-    document.getElementById('six')
-        .addEventListener('click', function() {
-            if (middle.value == "") {
-                lefty.value = (lefty.value + this.innerHTML)
-            } else if(middle.value == "="){
-                lefty.value = ("");
-                middle.value = ("");
-                righty.value = ("");
-                lefty.value = (lefty.value + this.innerHTML)
-            }else {
-                righty.value = (righty.value + this.innerHTML)
-            }
-        });
+var multiply = document.getElementById("multiplication")
+    .addEventListener('click', function () {
+        var RV = parseFloat(righty.value);
+        var LV = parseFloat(lefty.value);
+        middle.value = this.innerHTML;
+        if(lefty.value != "" && righty.value != "" && middle.value != "") {
+            lefty.value = RV * LV;
+            righty.value = "";
+        }
+    });
 
-    document.getElementById('seven')
-        .addEventListener('click', function() {
-            if (middle.value == "") {
-                lefty.value = (lefty.value + this.innerHTML)
-            } else if(middle.value == "="){
-                lefty.value = ("");
-                middle.value = ("");
-                righty.value = ("");
-                lefty.value = (lefty.value + this.innerHTML)
+var divide = document.getElementById("division")
+    .addEventListener('click', function () {
+        var RV = parseFloat(righty.value);
+        var LV = parseFloat(lefty.value);
+        middle.value = this.innerHTML;
+        if(lefty.value != "" && righty.value != "" && middle.value != "") {
+            lefty.value = RV / LV;
+            righty.value = "";
+        }
+    });
+var posOrNeg = document.getElementById("posNeg")
+    .addEventListener('click', function () {
+        if (middle.value == "") {
+            if(Math.abs(lefty.value) == lefty.value){
+                lefty.value = -Math.abs(lefty.value)
             }else {
-                righty.value = (righty.value + this.innerHTML)
+                lefty.value = Math.abs(lefty.value)
             }
-        });
-
-    document.getElementById('eight')
-        .addEventListener('click', function() {
-            if (middle.value == "") {
-                lefty.value = (lefty.value + this.innerHTML)
-            } else if(middle.value == "="){
-                lefty.value = ("");
-                middle.value = ("");
-                righty.value = ("");
-                lefty.value = (lefty.value + this.innerHTML)
-            }else {
-                righty.value = (righty.value + this.innerHTML)
-            }
-        });
-
-    document.getElementById('nine')
-        .addEventListener('click', function() {
-            if (middle.value == "") {
-                lefty.value = (lefty.value + this.innerHTML)
-            } else if(middle.value == "="){
-                lefty.value = ("");
-                middle.value = ("");
-                righty.value = ("");
-                lefty.value = (lefty.value + this.innerHTML)
-            }else {
-                righty.value = (righty.value + this.innerHTML)
-            }
-        });
-
-    document.getElementById('zero')
-        .addEventListener('click', function() {
-            if (middle.value == "") {
-                if(lefty.value.charAt(0) != "0") {
-                    lefty.value = (lefty.value + this.innerHTML)
-                }else if (lefty.value.charAt(0) == "0" && lefty.value.charAt(1) > "0") {
-                    lefty.value = (lefty.value + this.innerHTML)
-                }
-            } else if(middle.value == "="){
-                lefty.value = ("");
-                middle.value = ("");
-                righty.value = ("");
-                lefty.value = (lefty.value + this.innerHTML)
-            }else {
-                if(righty.value.charAt(0) != "0") {
-                    righty.value = (righty.value + this.innerHTML)
-                }else if (righty.value.charAt(0) == "0" && righty.value.charAt(1) > "0") {
-                    righty.value = (righty.value + this.innerHTML)
-                }
-            }
-        });
-
-    document.getElementById('decimal')
-        .addEventListener('click', function() {
-            if (middle.value == "") {
-                if(lefty.value % 1 == 0) {
-                    lefty.value = (lefty.value + this.innerHTML);
-                }
+        }else {
+            if (Math.abs(righty.value) == righty.value) {
+                righty.value = -Math.abs(righty.value)
             } else {
-                if(righty.value % 1 == 0) {
-                    righty.value = (righty.value + this.innerHTML);
-                }
+                righty.value = Math.abs(righty.value)
             }
-        });
+        }
+    });
 
-    var adds = document.getElementById("addition")
-        .addEventListener('click', function () {
-            var RV = parseFloat(righty.value);
-            var LV = parseFloat(lefty.value);
-            middle.value = this.innerHTML;
-            if(lefty.value != "" && righty.value != "" && middle.value != "") {
-                lefty.value = RV + LV;
-                righty.value = "";
-            }
-        });
-
-    var minus = document.getElementById("subtraction")
-        .addEventListener('click', function () {
-            var RV = parseFloat(righty.value);
-            var LV = parseFloat(lefty.value);
-            middle.value = this.innerHTML;
-            if(lefty.value != "" && righty.value != "" && middle.value != "") {
-                lefty.value = LV - RV;
-                righty.value = "";
-            }
-        });
-
-    var multiply = document.getElementById("multiplication")
-        .addEventListener('click', function () {
-            var RV = parseFloat(righty.value);
-            var LV = parseFloat(lefty.value);
-            middle.value = this.innerHTML;
-            if(lefty.value != "" && righty.value != "" && middle.value != "") {
-                lefty.value = RV * LV;
-                righty.value = "";
-            }
-        });
-
-    var divide = document.getElementById("division")
-        .addEventListener('click', function () {
-            var RV = parseFloat(righty.value);
-            var LV = parseFloat(lefty.value);
-            middle.value = this.innerHTML;
-            if(lefty.value != "" && righty.value != "" && middle.value != "") {
-                lefty.value = RV / LV;
-                righty.value = "";
-            }
-        });
-    var posOrNeg = document.getElementById("posNeg")
-        .addEventListener('click', function () {
-            if (middle.value == "") {
-                if(Math.abs(lefty.value) == lefty.value){
-                    lefty.value = -Math.abs(lefty.value)
-                }else {
-                    lefty.value = Math.abs(lefty.value)
-                }
-            }else {
-                if (Math.abs(righty.value) == righty.value) {
-                    righty.value = -Math.abs(righty.value)
-                } else {
-                    righty.value = Math.abs(righty.value)
-                }
-            }
-        });
-
-    var equation = document.getElementById("evaluate")
-        .addEventListener('click', function () {
-            var RV = parseFloat(righty.value);
-            var LV = parseFloat(lefty.value);
-            if(middle.value == "+") {
-                lefty.value = (RV + LV);
-                righty.value = "";
-                middle.value = "=";
-            }else if (middle.value == "-"){
-                lefty.value = (LV - RV);
-                righty.value = "";
-                middle.value = "=";
-            }else if (middle.value == "x"){
-                lefty.value = (LV * RV);
-                righty.value = "";
-                middle.value = "=";
-            }else if (middle.value == "÷"){
-                lefty.value = (LV / RV);
-                righty.value = "";
-                middle.value = "=";
-            }
-        });
+var equation = document.getElementById("evaluate")
+    .addEventListener('click', function () {
+        var RV = parseFloat(righty.value);
+        var LV = parseFloat(lefty.value);
+        if(middle.value == "+") {
+            lefty.value = (RV + LV);
+            righty.value = "";
+            middle.value = "=";
+        }else if (middle.value == "-"){
+            lefty.value = (LV - RV);
+            righty.value = "";
+            middle.value = "=";
+        }else if (middle.value == "x"){
+            lefty.value = (LV * RV);
+            righty.value = "";
+            middle.value = "=";
+        }else if (middle.value == "÷"){
+            lefty.value = (LV / RV);
+            righty.value = "";
+            middle.value = "=";
+        }
+    });
