28b66eb1a1d616f03315ef85b33a3ff617f2fab9
Lloyd Garrison <lloyd.h.garrison@gmail.com>
2016-10-26 15:29:03 -0500

    adding Ajax store and ajax blog exercises, animate exercise and sort-debug exerciese.

---
+++ codingChallenge.js
@@ -0,0 +1,30 @@
+/**
+ * Created by lloyd on 10/24/16.
+ */
+// latitude = 0 to 90
+// longitude = 0 to 180
+
+
+// var latLong = prompt("please enter your coordinates :");
+// latLongArray = latLong.split(",");
+// console.log(latLongArray);
+// for(var i; i < latLongArray.length; i++) {
+//     var long = Math.abs(latLong[0]);
+//     var lat = Math.abs(latLong[1]);
+//     if(lat )
+// }
+// alert("your latitude is: " + latLongArray[0] + " and " + latLongArray[1]);
+// function checkLatLong(long, lat) {
+//     if()
+//
+// }
+
+var inputs = "B%1A";
+for(var i; i < inputs.length; i++){
+    console.log(inputs.charCodeAt(i).toString(16));
+}
+// console.log("0xF1" + inputs.charCodeAt(0).toString(16));
+
+// data.forEach(function (input, index) {
+//     input[i] = input.charCodeAt(index).toString(16);
+// });
